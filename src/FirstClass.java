f4c39df43ff7b5f7126ef2f6798722ab65ce3816
Bodya <venomcob@gmail.com>
2019-03-28 20:08:40 +0400

    Сделал 4 задание

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,37 +1,47 @@
 class FirstClass {
 
     public  static void main (String [] args){
-        for (int i = 0; i < args.length; i++)
-            System.out.println(args[i]);
-
-    }
-    public class SecondClass {
-        private int i;
-        private int j;
-
-        public SecondClass(int i,int j){
-            this.i = i;
-            this.j = j;
-        }
-
-        public void setI(int i) {
-            this.i = i;
-        }
-
-        public void setJ(int j) {
-            this.j = j;
-        }
-
-        public int getI() {
-            return i;
-        }
-
-        public int getJ() {
-            return j;
-        }
-        public int plus(){
-            return i+j;
+        SecondClass o = new SecondClass(1,3);
+        int i, j;
+        for (i = 1; i <= 8; i++) {
+            for(j = 1; j <= 8; j++) {
+                o.setI(i);
+                o.setJ(j);
+                System.out.print(o.plus());
+                System.out.print(" ");
+            }
+            System.out.println();
         }
 
     }
+}
+
+class SecondClass {
+    private int i;
+    private int j;
+
+    public SecondClass(int i,int j){
+        this.i = i;
+        this.j = j;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public void setJ(int j) {
+        this.j = j;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public int getJ() {
+        return j;
+    }
+    public int plus(){
+        return i+j;
+    }
+
 }
