1a5f2444ff6d514c749bcf8f9aba2a3f1fea836a
Bodya <venomcob@gmail.com>
2019-03-28 19:54:47 +0400

    Initial commit

---
+++ src/FirstClass.java
@@ -0,0 +1,37 @@
+class FirstClass {
+
+    public  static void main (String [] args){
+        for (int i = 0; i < args.length; i++)
+            System.out.println(args[i]);
+
+    }
+    public class SecondClass {
+        private int i;
+        private int j;
+
+        public SecondClass(int i,int j){
+            this.i = i;
+            this.j = j;
+        }
+
+        public void setI(int i) {
+            this.i = i;
+        }
+
+        public void setJ(int j) {
+            this.j = j;
+        }
+
+        public int getI() {
+            return i;
+        }
+
+        public int getJ() {
+            return j;
+        }
+        public int plus(){
+            return i+j;
+        }
+
+    }
+}
